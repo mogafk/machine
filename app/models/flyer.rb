a41e9ce09f29709a2aed7bd69866b24ee61e7c0e
prosyakov <prosyakov@gmail.com>
2016-01-18 01:31:08 +0300

    some changes to imageprocessor

--- app/models/flyer.rb
+++ app/models/flyer.rb
@@ -1,13 +1,10 @@
 class Flyer < ActiveRecord::Base
 
-  has_attached_file :article, styles: 
-  			{thumb: "200x",
-  				small: "300x"
-  			}
+  has_attached_file :article, styles: {thumb: "300x", small: "200x"}
 
 
 
-  validates_attachment :article, content_type: { content_type: ["application/pdf", "image/jpg"] }
+  validates_attachment :article, content_type: { content_type: "application/pdf" }
   #validates_something_else # Other validations that conflict with Paperclip's
   belongs_to :posts
 end
